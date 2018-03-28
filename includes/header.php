f55291ceaeeeab299ef101bf0154d17355effcc6
Thodoris Pliakas <thodorispliakas@gmail.com>
2018-03-29 00:43:26 +0300

    added bootstrap classes to index file

--- includes/header.php
+++ includes/header.php
@@ -3,10 +3,9 @@
     <head>
         <meta charset="utf-8">
         <title>TechSupporter - <?php echo $pageTitle ?> </title>
-        <<meta charset="utf-8">
+        <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="shorcut icon" type="image/x-icon" href="images/favicon.png">
-        <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <link rel="stylesheet" href="css/main.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -2,16 +2,20 @@
     $pageTitle = 'Homepage';
     include('includes/header.php');
 ?>   
-        <nav class="col-xs-4">
-            <ul>
-                <a href="inhouse.php"><li id="inhouse"><b>In House</b></li></a>
-                <hr>
-                <a href="onsite.php"><li id="onsite"><b>On Site</b></li></a>
-                <hr>
-                <a href="clients.php"><li id="clients"><b>Clients</b></li></a>
-            </ul>
-        </nav>
-        <img id="mainimage" class="img-fluid" src="images/bg.jpg" alt="Tech Supporter">  
+    <div class="container">
+        <div class="row">
+            <nav class="col-xs-12">
+                <ul>
+                    <a href="inhouse.php"><li id="inhouse"><b>In House</b></li></a>
+                    <hr>
+                    <a href="onsite.php"><li id="onsite"><b>On Site</b></li></a>
+                    <hr>
+                    <a href="clients.php"><li id="clients"><b>Clients</b></li></a>
+                </ul>
+            </nav>
+            <img id="mainimage" class="img-fluid" src="images/bg.jpg" alt="Tech Supporter">
+        </div>
+    </div>
 <?php
     include('includes/footer.php');
 ?>
